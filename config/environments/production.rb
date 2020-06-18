ac50d77628427e25b8969e23e2234d8bf5e42c90
ex-chairperson <rqsmy292@yahoo.co.jp>
2020-06-18 12:22:42 +0000

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,12 +70,12 @@ Rails.application.configure do
   host = 'tweetapp2020.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
+    :address        => 'smtp.gmail.com',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :user_name      => ENV['GMAIL_USERNAME'],
+    :password       => ENV['GMAIL_PASSWORD'],
+    :domain         => 'gmail.com',
     :enable_starttls_auto => true
   }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
--- db/migrate/20200413073645_remove_plofile_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemovePlofileFromUsers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :plofile, :varchar
-  end
-end
